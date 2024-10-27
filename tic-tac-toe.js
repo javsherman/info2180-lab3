bf9e78bb32133e2f827365874d1be59fe29508c4
Javaughn <javsherman000@gmail.com>
2024-10-27 12:01:15 -0500

    Layout of Board

---
+++ tic-tac-toe.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const squares = document.querySelectorAll("#board div");
+    squares.forEach(square => {
+        square.classList.add("square");
+    });
+});
